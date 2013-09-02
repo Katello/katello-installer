d00bd5f6d138378e40a96e10ff2a4bb8e2fccaca
Ivan Nečas <inecas@redhat.com>
2013-09-02 18:37:40 -0400

    node-install extract the tar

--- modules/certs/manifests/tar_extract.pp
+++ modules/certs/manifests/tar_extract.pp
@@ -2,7 +2,6 @@ define certs::tar_extract($path = $title) {
   exec { "extract $path":
     cwd => '/root',
     path => ['/usr/bin', '/bin'],
-    command => "tar -xzf $path",
-    refreshonly => true
+    command => "tar -xzf $path"
   }
 }
