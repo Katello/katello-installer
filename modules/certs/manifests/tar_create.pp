c98d2f678e924bd289f1c219505b9258f69a6485
Ivan Necas <inecas@redhat.com>
2013-09-04 11:06:32 +0200

    Fix certs tar creation

--- modules/certs/manifests/tar_create.pp
+++ modules/certs/manifests/tar_create.pp
@@ -2,6 +2,6 @@ define certs::tar_create($path = $title, $child = $::certs::node_fqdn) {
   exec { "generate $path":
     cwd => '/root',
     path => ['/usr/bin', '/bin'],
-    command => "tar -czf $path /root/ssl-build/*.noarch.rpm /root/ssl-build/$child/*.noarch.rpm",
+    command => "tar -czf $path ssl-build/*.noarch.rpm ssl-build/$child/*.noarch.rpm",
   }
 }
