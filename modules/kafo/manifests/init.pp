0dc8fcdc41e417eb71ce3e8302291de6a7d886f5
Ivan Nečas <inecas@redhat.com>
2013-09-10 11:55:19 -0400

    first cut on foreman-proxy installation

--- modules/kafo/manifests/init.pp
+++ modules/kafo/manifests/init.pp
@@ -2,26 +2,83 @@
 #
 # === Parameters:
 #
-# $parent_fqdn:: fqdn of the parent node. REQUIRED
+# $parent_fqdn::            fqdn of the parent node. REQUIRED
 #
-# $pulp::        should Pulp be configured on the node
-#                type:boolean
+# $certs_tar::              path to a tar with certs for the node
 #
-# $pulp_admin_password:: passowrd for the Pulp admin user.It should be left blank so that random password is generated
-#                        type:password
+# $pulp::                   should Pulp be configured on the node
+#                           type:boolean
 #
-# $certs_tar::   path to a tar with certs for the node
+# $pulp_admin_password::    passowrd for the Pulp admin user.It should be left blank so that random password is generated
+#                           type:password
+#
+# $foreman_proxy_port::     Port on which will foreman proxy listen
+#                           type:integer
+#
+# $tftp::                   Use TFTP
+#                           type:boolean
+#
+# $dhcp::                   Use DHCP
+#                           type:boolean
+#
+# $dhcp_interface::         DHCP listen interface
+#
+# $dhcp_gateway::           DHCP pool gateway
+#
+# $dhcp_range::             Space-separated DHCP pool range
+#
+# $dhcp_nameservers::       DHCP nameservers
+#
+# $dns::                    Use DNS
+#                           type:boolean
+#
+# $dns_interface::          DNS interface
+#
+# $dns_forwarders::         DNS forwarders
+#                           type:array
+#
+# $register_in_foreman::    Register proxy back in Foreman
+#                           type:boolean
+#
+# $oauth_effective_user::   User to be used for REST interaction
+#
+# $oauth_consumer_key::     OAuth key to be used for REST interaction
+#
+# $oauth_consumer_secret::  OAuth secret to be used for REST interaction
+#
 #
 class kafo (
-  $parent_fqdn         = $kafo::params::parent_fqdn,
-  $pulp                = $kafo::params::pulp,
-  $pulp_admin_password = $kafo::params::pulp_admin_password,
-  $certs_tar           = $kafo::params::child_fqdn
+  $parent_fqdn           = $kafo::params::parent_fqdn,
+  $certs_tar             = $kafo::params::certs_tar,
+  $pulp                  = $kafo::params::pulp,
+  $pulp_admin_password   = $kafo::params::pulp_admin_password,
+
+  $foreman_proxy_port    = $kafo::params::foreman_proxy_port,
+
+  $tftp                  = $kafo::params::tftp,
+
+  $dhcp                  = $kafo::params::dhcp,
+  $dhcp_interface        = $kafo::params::dhcp_interface,
+  $dhcp_gateway          = $kafo::params::dhcp_gateway,
+  $dhcp_range            = $kafo::params::dhcp_range,
+  $dhcp_nameservers      = $kafo::params::dhcp_nameservers,
+
+  $dns                   = $kafo::params::dns,
+  $dns_interface         = $kafo::params::dns_interface,
+  $dns_forwarders        = $kafo::params::dns_forwarders,
+
+  $register_in_foreman   = $kafo::params::register_in_foreman,
+  $oauth_effective_user  = $kafo::params::oauth_effective_user,
+  $oauth_consumer_key    = $kafo::params::oauth_consumer_key,
+  $oauth_consumer_secret = $kafo::params::oauth_consumer_secret
   ) inherits kafo::params {
 
   validate_present($parent_fqdn)
-  validate_file_exists($certs_tar)
-  validate_pulp($pulp)
+
+  if $pulp {
+    validate_pulp($pulp)
+    validate_file_exists($certs_tar)
+  }
 
   if $certs_tar {
     certs::tar_extract { $certs_tar:
@@ -39,9 +96,34 @@ class kafo (
     class { 'pulp::child':
       parent_fqdn => $parent_fqdn
     }
+    katello_node { "https://${parent_fqdn}/katello":
+      content => $pulp
+    }
   }
 
-  katello_node { "https://${parent_fqdn}/katello":
-    content => $pulp
+  if $tftp or $dhcp or $dns {
+    class { foreman_proxy:
+     custom_repo           => true,
+     port                  => $foreman_proxy_port,
+     puppetca              => false,
+     ssl_ca                => false,
+     ssl_cert              => false,
+     ssl_key               => false,
+     tftp                  => $tftp,
+     dhcp                  => $dhcp,
+     dhcp_interface        => $dhcp_interface,
+     dhcp_gateway          => $dhcp_gateway,
+     dhcp_range            => $dhcp_range,
+     dhcp_nameservers      => $dhcp_nameservers,
+     dns                   => $dns,
+     dns_interface         => $dns_interface,
+     dns_forwarders        => $dns_forwarders,
+     register_in_foreman   => $register_in_foreman,
+     foreman_base_url      => "https://$parent_fqdn/foreman",
+     registered_proxy_url  => "http://${fqdn}:${foreman_proxy_port}",
+     oauth_effective_user  => $oauth_effective_user,
+     oauth_consumer_key    => $oauth_consumer_key,
+     oauth_consumer_secret => $oauth_consumer_secret
+    }
   }
 }
--- modules/kafo/manifests/params.pp
+++ modules/kafo/manifests/params.pp
@@ -1,6 +1,24 @@
 class kafo::params {
-  $child_fqdn          = undef
-  $master_fqdn         = undef
-  $pulp                = true
-  $pulp_admin_password = undef
+
+  include foreman_proxy::params
+
+  $parent_fqdn           = undef
+  $child_fqdn            = undef
+  $certs_tar             = undef
+  $pulp                  = true
+  $pulp_admin_password   = undef
+
+  $foreman_proxy_port    = "9090"
+  $dhcp                  = $foreman_proxy::params::dhcp
+  $dhcp_interface        = $foreman_proxy::params::dhcp_interface
+  $dhcp_gateway          = $foreman_proxy::params::dhcp_gateway
+  $dhcp_range            = $foreman_proxy::params::dhcp_range
+  $dhcp_nameservers      = $foreman_proxy::params::dhcp_nameservers
+  $dns                   = $foreman_proxy::params::dns
+  $dns_interface         = $foreman_proxy::params::dns_interface
+  $dns_forwareders       = $foreman_proxy::params::dns_forwarders
+  $register_in_foreman   = $foreman_proxy::params::register_in_foreman
+  $oauth_effective_user  = $foreman_proxy::params::oauth_effective_user
+  $oauth_consumer_key    = "foreman"
+  $oauth_consumer_secret = $foreman_proxy::params::secret
 }
