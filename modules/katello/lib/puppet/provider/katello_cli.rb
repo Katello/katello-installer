496d24316d6114339eac555cc8ab012c0a2e3915
Ivan Necas <inecas@redhat.com>
2013-09-23 16:40:30 +0200

    Sanitize Katello output

--- modules/katello/lib/puppet/provider/katello_cli.rb
+++ modules/katello/lib/puppet/provider/katello_cli.rb
@@ -7,9 +7,9 @@ class Puppet::Provider::KatelloCli < Puppet::Provider
   commands :katello_cli => 'katello'
 
   def katello(*args)
-    katello_cli("--username", resource[:user],
-                "--password", resource[:password],
-               *args)
+    sanitize_output(katello_cli("--username", resource[:user],
+                                "--password", resource[:password],
+                                *args))
   end
 
   def katello_list(*args)
@@ -17,6 +17,12 @@ class Puppet::Provider::KatelloCli < Puppet::Provider
     katello(*args).lines.map do |line|
       line.split(';')
     end
+  end
+
+  # sometimes some control characters appear in the katello cli output. Get rid
+  # of them
+  def sanitize_output(output)
+    output.sub(/\e\[\?\d+h/,'')
   end
 
 end
