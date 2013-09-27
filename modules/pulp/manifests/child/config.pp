48835d679d7f1a1c33c35314655639cd665a3a47
Ivan Necas <inecas@redhat.com>
2013-09-27 13:46:58 +0200

    Make sure goferd is restarted when server.conf changes

--- modules/pulp/manifests/child/config.pp
+++ modules/pulp/manifests/child/config.pp
@@ -5,4 +5,7 @@ class pulp::child::config {
      content => template("pulp/etc/pulp/nodes.conf.erb"),
   }
 
+  # we need to make sure the goferd reads the current oauth credentials to talk
+  # to the child node
+  File['/etc/pulp/server.conf'] ~> Service['goferd']
 }
