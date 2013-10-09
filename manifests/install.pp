dbc1a710ca4f65b6b7d45d432a1fc79de114ee39
Ivan Necas <inecas@redhat.com>
2013-10-09 10:20:34 +0200

    Squashed 'modules/passenger/' changes from 61b42f3..5ed0b9c
    5ed0b9c Release 1.3.0-rc4
    83d973c Release 1.3.0-rc3
    4d4c1ed Add Amazon Linux support
    712f5b6 Do not install the repo by default
    
    git-subtree-dir: modules/passenger
    git-subtree-split: 5ed0b9ca3161720095bc9a5355aed6ab87739757

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,13 +1,38 @@
 class passenger::install {
   case $::osfamily {
     RedHat: {
-      include passenger::install::redhat
+      $package_name = 'mod_passenger'
     }
     Debian: {
-      include passenger::install::debian
+      $package_name = 'libapache2-mod-passenger'
+
+      # Enable RequestHeaders
+      exec { 'enable-headers':
+        command => '/usr/sbin/a2enmod headers',
+        creates => '/etc/apache2/mods-enabled/headers.load',
+        require => Class['apache::install'],
+        notify  => Class['apache::service'],
+      }
+    }
+    Linux: {
+      case $::operatingsystem {
+        Amazon: {
+          $package_name = 'mod_passenger'
+        }
+        default: {
+          fail("${::hostname}: This module does not support operatingsystem ${::operatingsystem}")
+        }
+      }
     }
     default: {
-      fail("${::hostname}: This module does not support operatingsystem ${::osfamily}")
+      fail("${::hostname}: This module does not support osfamily ${::osfamily}")
     }
+  }
+
+  package { 'passenger':
+    ensure  => installed,
+    name    => $package_name,
+    require => Class['apache::install'],
+    before  => Class['apache::service'],
   }
 }
--- spec/classes/passenger_install_spec.rb
+++ spec/classes/passenger_install_spec.rb
@@ -9,8 +9,13 @@ describe 'passenger' do
       }
     end
 
-    it 'should include redhat install' do
-      should include_class('passenger::install::redhat')
+    it 'should install passenger' do
+      should contain_package('passenger').with({
+        :ensure  => 'installed',
+        :name    => 'mod_passenger',
+        :require => 'Class[Apache::Install]',
+        :before  => 'Class[Apache::Service]',
+      })
     end
   end
 
@@ -21,8 +26,54 @@ describe 'passenger' do
       }
     end
 
-    it 'should include debian install' do
-      should include_class('passenger::install::debian')
+    it 'should install passenger' do
+      should contain_package('passenger').with({
+        :ensure  => 'installed',
+        :name    => 'libapache2-mod-passenger',
+        :require => 'Class[Apache::Install]',
+        :before  => 'Class[Apache::Service]',
+      })
+    end
+
+    it 'should enable RequestHeaders' do
+      should contain_exec('enable-headers').with({
+        :command => '/usr/sbin/a2enmod headers',
+        :creates => '/etc/apache2/mods-enabled/headers.load',
+        :require => 'Class[Apache::Install]',
+        :notify  => 'Class[Apache::Service]',
+      })
+    end
+  end
+
+  context 'on Amazon Linux' do
+    let :facts do
+      {
+        :osfamily        => 'Linux',
+        :operatingsystem => 'Amazon',
+      }
+    end
+
+    it 'should install passenger' do
+      should contain_package('passenger').with({
+        :ensure  => 'installed',
+        :name    => 'mod_passenger',
+        :require => 'Class[Apache::Install]',
+        :before  => 'Class[Apache::Service]',
+      })
+    end
+  end
+
+  context 'on unsupported Linux operatingsystem' do
+    let :facts do
+      {
+        :hostname        => 'localhost',
+        :osfamily        => 'Linux',
+        :operatingsystem => 'Unsupported',
+      }
+    end
+
+    it 'should fail' do
+      expect { subject }.to raise_error(/#{facts[:hostname]}: This module does not support operatingsystem #{facts[:operatingsystem]}/)
     end
   end
 
@@ -35,7 +86,7 @@ describe 'passenger' do
     end
 
     it 'should fail' do
-      expect { subject }.to raise_error(/#{facts[:hostname]}: This module does not support operatingsystem #{facts[:osfamily]}/)
+      expect { subject }.to raise_error(/#{facts[:hostname]}: This module does not support osfamily #{facts[:osfamily]}/)
     end
   end
 end
