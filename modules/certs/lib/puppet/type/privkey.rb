1dcfeb51a14e9858a7c5ccc7da9b8547b7ab4b63
Ivan Necas <inecas@redhat.com>
2013-10-23 10:52:53 +0200

    Prepare for adding certs module as subtree

--- modules/certs/lib/puppet/type/privkey.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path('../certs_common', __FILE__)
-
-Puppet::Type.newtype(:privkey) do
-  desc 'Stores the private key file on a location'
-
-  instance_eval(&Certs::FILE_COMMON_PARAMS)
-end
--- modules/certs/manifests/tar_extract.pp
+++
@@ -1,7 +0,0 @@
-define certs::tar_extract($path = $title) {
-  exec { "extract $path":
-    cwd => '/root',
-    path => ['/usr/bin', '/bin'],
-    command => "tar -xzf $path"
-  }
-}
