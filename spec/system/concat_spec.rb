puppetlabs/puppetlabs-concat

commit 3d60c9ef6162425c0bf9eb2004d9da375965efc9

Refactor loop to use for-do instead of while-read.
This for loop around find removes the need for the read statement.
The read statement isn't portable to solaris, because Read has no -r option
in /bin/sh on solaris. This should resolve Issue #45.

files/concatfragments.sh
@@ -112,9 +112,14 @@ else
 fi
 
 # find all the files in the fragments directory, sort them numerically and concat to fragments.concat in the working dir
-find fragments/ -type f -follow | LANG=C sort ${SORTARG} | while read -r fragfile; do
-	cat "$fragfile" >> "fragments.concat"
+IFS_BACKUP=$IFS
+IFS='
+'
+for fragfile in `find fragments/ -type f -follow | LANG=C sort ${SORTARG}`
+do
+    cat $fragfile >> concat.fragment
 done
+IFS=$IFS_BACKUP
 
 if [ x${TEST} = "x" ]; then
 	# This is a real run, copy the file to outfile

Katello/katello-installer

commit 8ddfab3947bc7b6f66bad6518285639535417498

Squashed 'modules/concat/' changes from 57ce1c7..0435697
0435697 Merge pull request #65 from apenney/100-release
8584aa9 Release 1.0.0.
562919f Merge pull request #64 from apenney/100-release
5423710 Release 1.0.0.
7313d26 Merge pull request #61 from hubspotdevops/newline_upstream
68e5e73 Merge pull request #63 from plantigrade/master
1830a68 Fix spec tests
3728ba3 Update concatfragments.sh ... fix broken behavior introduced in 3d60c9ef6162425c0bf9eb2004d9da375965efc9
871e0b7 new_line -> newline
5b48399 Add -l to getopts
3defb6e Ability to control if fragments should always have a newline at the end.
0046fe4 Merge pull request #56 from nibalizer/for-loop
3d60c9e Refactor loop to use for-do instead of while-read.
2b2d974 Merge pull request #60 from apenney/rspec-system
1d403b6 Add rspec-system tests.
82de49b Merge pull request #46 from andir/warnmsg_fix
fe537c0 Merge pull request #51 from duritong/lang_sort
78e5470 sort always with LANG=C
c54a95f Merge pull request #50 from tremble/master
688655e RDoc cleanup
f1579e5 Allow WARNMSG to contain/start with '#'

git-subtree-dir: modules/concat
git-subtree-split: 04356974f72b90a1d0f57346a00e95a717924e43

.nodeset.yml
@@ -1,0 +1,35 @@
+---
+default_set: 'centos-64-x64'
+sets:
+  'centos-59-x64':
+    nodes:
+      "main.foo.vm":
+        prefab: 'centos-59-x64'
+  'centos-64-x64':
+    nodes:
+      "main.foo.vm":
+        prefab: 'centos-64-x64'
+  'fedora-18-x64':
+    nodes:
+      "main.foo.vm":
+        prefab: 'fedora-18-x64'
+  'debian-607-x64':
+    nodes:
+      "main.foo.vm":
+        prefab: 'debian-607-x64'
+  'debian-70rc1-x64':
+    nodes:
+      "main.foo.vm":
+        prefab: 'debian-70rc1-x64'
+  'ubuntu-server-10044-x64':
+    nodes:
+      "main.foo.vm":
+        prefab: 'ubuntu-server-10044-x64'
+  'ubuntu-server-12042-x64':
+    nodes:
+      "main.foo.vm":
+        prefab: 'ubuntu-server-12042-x64'
+  'sles-11sp1-x64':
+    nodes:
+      "main.foo.vm":
+        prefab: 'sles-11sp1-x64'

.travis.yml
@@ -1,12 +1,32 @@
-language: ruby
-rvm:
-  - 1.8.7
-script:
-  - "rake lint"
-  - "rake spec"
+---
 branches:
   only:
     - master
+language: ruby
+bundler_args: --without development
+script: bundle exec rake spec SPEC_OPTS='--format documentation'
+after_success:
+  - git clone -q git://github.com/puppetlabs/ghpublisher.git .forge-releng
+  - .forge-releng/publish
+rvm:
+  - 1.8.7
+  - 1.9.3
+  - 2.0.0
 env:
-  - PUPPET_VERSION=2.7.11
-gemfile: .gemfile
+  matrix:
+    - PUPPET_GEM_VERSION="~> 2.7.0" FACTER_GEM_VERSION="~> 1.6.0"
+    - PUPPET_GEM_VERSION="~> 2.7.0" FACTER_GEM_VERSION="~> 1.7.0"
+    - PUPPET_GEM_VERSION="~> 3.2.0"
+  global:
+matrix:
+  exclude:
+    - rvm: 1.9.3
+      env: PUPPET_GEM_VERSION="~> 2.7.0" FACTER_GEM_VERSION="~> 1.6.0"
+    - rvm: 1.9.3
+      env: PUPPET_GEM_VERSION="~> 2.7.0" FACTER_GEM_VERSION="~> 1.7.0"
+    - rvm: 2.0.0
+      env: PUPPET_GEM_VERSION="~> 2.7.0" FACTER_GEM_VERSION="~> 1.6.0"
+    - rvm: 2.0.0
+      env: PUPPET_GEM_VERSION="~> 2.7.0" FACTER_GEM_VERSION="~> 1.7.0"
+notifications:
+  email: false

Gemfile
@@ -1,0 +1,21 @@
+source 'https://rubygems.org'
+
+group :development, :test do
+  gem 'rake',                    :require => false
+  gem 'rspec-puppet',            :require => false
+  gem 'puppetlabs_spec_helper',  :require => false
+  gem 'rspec-system-puppet',     :require => false
+  gem 'puppet-lint',             :require => false
+  gem 'serverspec',              :require => false
+  gem 'rspec-system-serverspec', :require => false
+  gem 'pry',                     :require => false
+  gem 'vagrant-wrapper',         :require => false
+end
+
+if puppetversion = ENV['PUPPET_GEM_VERSION']
+  gem 'puppet', puppetversion, :require => false
+else
+  gem 'puppet', :require => false
+end
+
+# vim:ft=ruby

Modulefile
@@ -1,8 +1,8 @@
-name 'ripienaar-concat'
-version '0.2.0'
-source 'git://github.com/ripienaar/puppet-concat.git'
-author 'R.I.Pienaar'
-license 'Apache'
+name 'puppetlabs-concat'
+version '1.0.0'
+source 'git://github.com/puppetlabs/puppetlabs-concat.git'
+author 'Puppetlabs'
+license 'Apache 2.0'
 summary 'Concat module'
 description 'Concat module'
-project_page 'http://github.com/ripienaar/puppet-concat'
+project_page 'http://github.com/puppetlabs/puppetlabs-concat'

README
@@ -1,0 +1,91 @@
+== Module: concat
+
+A system to construct files using fragments from other files or templates.
+
+This requires at least puppet 0.25 to work correctly as we use some
+enhancements in recursive directory management and regular expressions
+to do the work here.
+
+=== Usage:
+
+The basic use case is as below:
+
+ concat{"/etc/named.conf":
+   notify => Service["named"]
+ }
+
+ concat::fragment{"foo.com_config":
+   target  => "/etc/named.conf",
+   order   => 10,
+   content => template("named_conf_zone.erb")
+ }
+
+ # add a fragment not managed by puppet so local users
+ # can add content to managed file
+ concat::fragment{"foo.com_user_config":
+   target  => "/etc/named.conf",
+   order   => 12,
+   ensure  => "/etc/named.conf.local"
+ }
+
+This will use the template named_conf_zone.erb to build a single
+bit of config up and put it into the fragments dir.  The file
+will have an number prefix of 10, you can use the order option
+to control that and thus control the order the final file gets built in.
+
+You can also specify a path and use a different name for your resources:
+
+ # You can make this something dynamic, based on whatever parameters your
+ # module/class for example.
+ $vhost_file = '/etc/httpd/vhosts/01-my-vhost.conf'
+
+ concat{'apache-vhost-myvhost':
+  path => $vhost_file,
+ }
+
+ # We don't care where the file is located, just what to put in it.
+ concat::fragment {'apache-vhost-myvhost-main':
+  target  => 'apache-vhost-myvhost',
+  content => '<virtualhost *:80>',
+  order   => 01,
+ }
+
+ concat::fragment {'apache-vhost-myvhost-close':
+  target  => 'apache-vhost-myvhost',
+  content => '</virtualhost>',
+  order   => 99,
+ }
+
+=== Setup:
+
+The class concat::setup uses the fact concat_basedir to define the variable
+$concatdir, where all the temporary files and fragments will be
+durably stored. The fact concat_basedir will be set up on the client to
+<Puppet[:vardir]>/concat, so you will be able to run different setup/flavours
+of puppet clients.
+However, since this requires the file lib/facter/concat_basedir.rb to be
+deployed on the clients, so you will have to set "pluginsync = true" on
+both the master and client, at least for the first run.
+
+There's some regular expression magic to figure out the puppet version but
+if you're on an older 0.24 version just set $puppetversion = 24
+
+=== Detail:
+
+We use a helper shell script called concatfragments.sh that gets placed
+in <Puppet[:vardir]>/concat/bin to do the concatenation.  While this might
+seem more complex than some of the one-liner alternatives you might find on
+the net we do a lot of error checking and safety checks in the script to avoid
+problems that might be caused by complex escaping errors etc.
+
+=== License:
+
+Apache Version 2
+
+=== Latest:
+
+http://github.com/puppetlabs/puppetlabs-concat/
+
+=== Contact:
+
+Puppetlabs, via our puppet-users@ mailing list.

README.markdown
@@ -151,4 +151,4 @@ Contributors:
 
 Contact:
 --------
-R.I.Pienaar / rip@devco.net / @ripienaar / http://devco.net
+puppet-users@ mailing list.

Rakefile
@@ -1,2 +1,2 @@
-require 'rubygems'
 require 'puppetlabs_spec_helper/rake_tasks'
+require 'rspec-system/rake_task'

files/concatfragments.sh
@@ -44,6 +44,7 @@ TEST=""
 FORCE=""
 WARN=""
 SORTARG=""
+ENSURE_NEWLINE=""
 
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
@@ -51,7 +52,7 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin
 ## http://nexenta.org/projects/site/wiki/Personalities
 unset SUN_PERSONALITY
 
-while getopts "o:s:d:tnw:f" options; do
+while getopts "o:s:d:tnw:fl" options; do
 	case $options in
 		o ) OUTFILE=$OPTARG;;
 		d ) WORKDIR=$OPTARG;;
@@ -59,6 +60,7 @@ while getopts "o:s:d:tnw:f" options; do
 		w ) WARNMSG="$OPTARG";;
 		f ) FORCE="true";;
 		t ) TEST="true";;
+		l ) ENSURE_NEWLINE="true";;
 		* ) echo "Specify output file with -o and fragments directory with -d"
 		    exit 1;;
 	esac
@@ -105,16 +107,25 @@ fi
 
 cd ${WORKDIR}
 
-if [ x${WARNMSG} = "x" ]; then
+if [ "x${WARNMSG}" = "x" ]; then
 	: > "fragments.concat"
 else
 	printf '%s\n' "$WARNMSG" > "fragments.concat"
 fi
 
+if [ x${ENSURE_NEWLINE} != x ]; then
+	find fragments/ -type f -follow -print0 | xargs -0 -I '{}' sh -c 'if [ -n "$(tail -c 1 < {} )" ]; then echo >> {} ; fi'
+fi
+
 # find all the files in the fragments directory, sort them numerically and concat to fragments.concat in the working dir
-find fragments/ -type f -follow | sort ${SORTARG} | while read -r fragfile; do
-	cat "$fragfile" >> "fragments.concat"
+IFS_BACKUP=$IFS
+IFS='
+'
+for fragfile in `find fragments/ -type f -follow | LANG=C sort ${SORTARG}`
+do
+    cat $fragfile >> "fragments.concat"
 done
+IFS=$IFS_BACKUP
 
 if [ x${TEST} = "x" ]; then
 	# This is a real run, copy the file to outfile

lib/facter/concat_basedir.rb
@@ -1,3 +1,9 @@
+# == Fact: concat_basedir
+#
+# A custom fact that sets the default location for fragments
+#
+# "${::vardir}/concat/"
+#
 Facter.add("concat_basedir") do
     setcode do
         File.join(Puppet[:vardir],"concat")

manifests/fragment.pp
@@ -1,19 +1,40 @@
+# == Define: concat::fragment
+#
 # Puts a file fragment into a directory previous setup using concat
 #
-# OPTIONS:
-#   - target    The file that these fragments belong to
-#   - content   If present puts the content into the file
-#   - source    If content was not specified, use the source
-#   - order     By default all files gets a 10_ prefix in the directory
-#               you can set it to anything else using this to influence the
-#               order of the content in the file
-#   - ensure    Present/Absent or destination to a file to include another file
-#   - mode      Mode for the file
-#   - owner     Owner of the file
-#   - group     Owner of the file
-#   - backup    Controls the filebucketing behavior of the final file and
-#               see File type reference for its use.  Defaults to 'puppet'
-define concat::fragment($target, $content=undef, $source=undef, $order=10, $ensure = 'present', $mode = '0644', $owner = $::id, $group = $concat::setup::root_group, $backup = 'puppet') {
+# === Options:
+#
+# [*target*]
+#   The file that these fragments belong to
+# [*content*]
+#   If present puts the content into the file
+# [*source*]
+#   If content was not specified, use the source
+# [*order*]
+#   By default all files gets a 10_ prefix in the directory you can set it to
+#   anything else using this to influence the order of the content in the file
+# [*ensure*]
+#   Present/Absent or destination to a file to include another file
+# [*mode*]
+#   Mode for the file
+# [*owner*]
+#   Owner of the file
+# [*group*]
+#   Owner of the file
+# [*backup*]
+#   Controls the filebucketing behavior of the final file and see File type
+#   reference for its use.  Defaults to 'puppet'
+#
+define concat::fragment(
+    $target,
+    $content=undef,
+    $source=undef,
+    $order=10,
+    $ensure = 'present',
+    $mode = '0644',
+    $owner = $::id,
+    $group = $concat::setup::root_group,
+    $backup = 'puppet') {
   $safe_name = regsubst($name, '[/\n]', '_', 'GM')
   $safe_target_name = regsubst($target, '[/\n]', '_', 'GM')
   $concatdir = $concat::setup::concatdir

manifests/init.pp
@@ -1,128 +1,50 @@
-# A system to construct files using fragments from other files or templates.
+# == Define: concat
 #
-# This requires at least puppet 0.25 to work correctly as we use some
-# enhancements in recursive directory management and regular expressions
-# to do the work here.
-#
-# USAGE:
-# The basic use case is as below:
-#
-# concat{"/etc/named.conf":
-#    notify => Service["named"]
-# }
-#
-# concat::fragment{"foo.com_config":
-#    target  => "/etc/named.conf",
-#    order   => 10,
-#    content => template("named_conf_zone.erb")
-# }
-#
-# # add a fragment not managed by puppet so local users
-# # can add content to managed file
-# concat::fragment{"foo.com_user_config":
-#    target  => "/etc/named.conf",
-#    order   => 12,
-#    ensure  => "/etc/named.conf.local"
-# }
-#
-# This will use the template named_conf_zone.erb to build a single
-# bit of config up and put it into the fragments dir.  The file
-# will have an number prefix of 10, you can use the order option
-# to control that and thus control the order the final file gets built in.
-#
-# You can also specify a path and use a different name for your resources:
-#
-# # You can make this something dynamic, based on whatever parameters your
-# # module/class for example.
-# $vhost_file = '/etc/httpd/vhosts/01-my-vhost.conf'
-#
-# concat{'apache-vhost-myvhost':
-#   path => $vhost_file,
-# }
-#
-# # We don't care where the file is located, just what to put in it.
-# concat::fragment {'apache-vhost-myvhost-main':
-#   target  => 'apache-vhost-myvhost',
-#   content => '<virtualhost *:80>',
-#   order   => 01,
-# }
-#
-# concat::fragment {'apache-vhost-myvhost-close':
-#   target  => 'apache-vhost-myvhost',
-#   content => '</virtualhost>',
-#   order   => 99,
-# }
-#
-#
-# SETUP:
-# The class concat::setup uses the fact concat_basedir to define the variable
-# $concatdir, where all the temporary files and fragments will be
-# durably stored. The fact concat_basedir will be set up on the client to
-# <Puppet[:vardir]>/concat, so you will be able to run different setup/flavours
-# of puppet clients.
-# However, since this requires the file lib/facter/concat_basedir.rb to be
-# deployed on the clients, so you will have to set "pluginsync = true" on
-# both the master and client, at least for the first run.
-#
-# There's some regular expression magic to figure out the puppet version but
-# if you're on an older 0.24 version just set $puppetversion = 24
-#
-# DETAIL:
-# We use a helper shell script called concatfragments.sh that gets placed
-# in <Puppet[:vardir]>/concat/bin to do the concatenation.  While this might
-# seem more complex than some of the one-liner alternatives you might find on
-# the net we do a lot of error checking and safety checks in the script to avoid
-# problems that might be caused by complex escaping errors etc.
-#
-# LICENSE:
-# Apache Version 2
-#
-# LATEST:
-# http://github.com/ripienaar/puppet-concat/
-#
-# CONTACT:
-# R.I.Pienaar <rip@devco.net>
-# Volcane on freenode
-# @ripienaar on twitter
-# www.devco.net
-
-
 # Sets up so that you can use fragments to build a final config file,
 #
-# OPTIONS:
-#  - path       The path to the final file. Use this in case you want to
-#               differentiate between the name of a resource and the file path.
-#               Note: Use the name you provided in the target of your
-#               fragments.
-#  - mode       The mode of the final file
-#  - owner      Who will own the file
-#  - group      Who will own the file
-#  - force      Enables creating empty files if no fragments are present
-#  - warn       Adds a normal shell style comment top of the file indicating
-#               that it is built by puppet
-#  - backup     Controls the filebucketing behavior of the final file and
-#               see File type reference for its use.  Defaults to 'puppet'
-#  - replace    Whether to replace a file that already exists on the local
-#               system
+# === Options:
 #
-# ACTIONS:
-#  - Creates fragment directories if it didn't exist already
-#  - Executes the concatfragments.sh script to build the final file, this
-#    script will create directory/fragments.concat.   Execution happens only
-#    when:
-#    * The directory changes
-#    * fragments.concat != final destination, this means rebuilds will happen
-#      whenever someone changes or deletes the final file.  Checking is done
-#      using /usr/bin/cmp.
-#    * The Exec gets notified by something else - like the concat::fragment
-#      define
-#  - Copies the file over to the final destination using a file resource
+# [*path*]
+#   The path to the final file. Use this in case you want to differentiate
+#   between the name of a resource and the file path.  Note: Use the name you
+#   provided in the target of your fragments.
+# [*mode*]
+#   The mode of the final file
+# [*owner*]
+#   Who will own the file
+# [*group*]
+#   Who will own the file
+# [*force*]
+#   Enables creating empty files if no fragments are present
+# [*warn*]
+#   Adds a normal shell style comment top of the file indicating that it is
+#   built by puppet
+# [*backup*]
+#   Controls the filebucketing behavior of the final file and see File type
+#   reference for its use.  Defaults to 'puppet'
+# [*replace*]
+#   Whether to replace a file that already exists on the local system
 #
-# ALIASES:
-#  - The exec can notified using Exec["concat_/path/to/file"] or
-#    Exec["concat_/path/to/directory"]
-#  - The final file can be referened as File["/path/to/file"] or
-#    File["concat_/path/to/file"]
+# === Actions:
+# * Creates fragment directories if it didn't exist already
+# * Executes the concatfragments.sh script to build the final file, this
+#   script will create directory/fragments.concat.   Execution happens only
+#   when:
+#   * The directory changes
+#   * fragments.concat != final destination, this means rebuilds will happen
+#     whenever someone changes or deletes the final file.  Checking is done
+#     using /usr/bin/cmp.
+#   * The Exec gets notified by something else - like the concat::fragment
+#     define
+# * Copies the file over to the final destination using a file resource
+#
+# === Aliases:
+#
+# * The exec can notified using Exec["concat_/path/to/file"] or
+#   Exec["concat_/path/to/directory"]
+# * The final file can be referened as File["/path/to/file"] or
+#   File["concat_/path/to/file"]
+#
 define concat(
   $path = $name,
   $owner = $::id,
@@ -133,7 +55,8 @@ define concat(
   $backup = 'puppet',
   $replace = true,
   $gnu = undef,
-  $order='alpha'
+  $order='alpha',
+  $ensure_newline = false
 ) {
   include concat::setup
 
@@ -186,6 +109,18 @@ define concat(
     }
   }
 
+  case $ensure_newline {
+    'true', true, yes, on: {
+      $newlineflag = '-l'
+    }
+    'false', false, no, off: {
+      $newlineflag = ''
+    }
+    default: {
+      fail("Improper 'ensure_newline' value given to concat: ${ensure_newline}")
+    }
+  }
+
   File {
     owner   => $::id,
     group   => $group,
@@ -233,7 +168,7 @@ define concat(
 
   exec { "concat_${name}":
     alias       => "concat_${fragdir}",
-    command     => "${concat::setup::concatdir}/bin/concatfragments.sh -o ${fragdir}/${concat_name} -d ${fragdir} ${warnflag} ${forceflag} ${orderflag}",
+    command     => "${concat::setup::concatdir}/bin/concatfragments.sh -o ${fragdir}/${concat_name} -d ${fragdir} ${warnflag} ${forceflag} ${orderflag} ${newlineflag}",
     notify      => File[$name],
     require     => [
       File[$fragdir],
@@ -241,7 +176,7 @@ define concat(
       File["${fragdir}/fragments.concat"],
     ],
     subscribe   => File[$fragdir],
-    unless      => "${concat::setup::concatdir}/bin/concatfragments.sh -o ${fragdir}/${concat_name} -d ${fragdir} -t ${warnflag} ${forceflag} ${orderflag}",
+    unless      => "${concat::setup::concatdir}/bin/concatfragments.sh -o ${fragdir}/${concat_name} -d ${fragdir} -t ${warnflag} ${forceflag} ${orderflag} ${newlineflag}",
   }
 
   if $::id == 'root' {

manifests/setup.pp
@@ -1,17 +1,22 @@
+# === Class: concat::setup
+#
 # Sets up the concat system.
 #
-# $concatdir is where the fragments live and is set on the fact concat_basedir.
-# Since puppet should always manage files in $concatdir and they should
-# not be deleted ever, /tmp is not an option.
+# [$concatdir]
+#   is where the fragments live and is set on the fact concat_basedir.
+#   Since puppet should always manage files in $concatdir and they should
+#   not be deleted ever, /tmp is not an option.
 #
-# $puppetversion should be either 24 or 25 to enable a 24 compatible
-# mode, in 24 mode you might see phantom notifies this is a side effect
-# of the method we use to clear the fragments directory.
+# [$puppetversion]
+#   should be either 24 or 25 to enable a 24 compatible
+#   mode, in 24 mode you might see phantom notifies this is a side effect
+#   of the method we use to clear the fragments directory.
 #
 # The regular expression below will try to figure out your puppet version
 # but this code will only work in 0.24.8 and newer.
 #
 # It also copies out the concatfragments.sh file to ${concatdir}/bin
+#
 class concat::setup {
   $id = $::id
   $root_group = $id ? {

spec/defines/init_spec.rb
@@ -49,7 +49,7 @@ describe 'concat' do
     should contain_exec("concat_/etc/foo.bar").with_command(
       "#{basedir}/bin/concatfragments.sh " +
       "-o #{basedir}/_etc_foo.bar/fragments.concat.out " +
-      "-d #{basedir}/_etc_foo.bar   "
+      "-d #{basedir}/_etc_foo.bar    "
     )
   end
 end
@@ -105,7 +105,7 @@ describe 'concat' do
     should contain_exec("concat_foobar").with_command(
       "#{basedir}/bin/concatfragments.sh " +
       "-o #{basedir}/foobar/fragments.concat.out " +
-      "-d #{basedir}/foobar   "
+      "-d #{basedir}/foobar    "
     )
   end
 

spec/spec_helper_system.rb
@@ -1,0 +1,25 @@
+require 'rspec-system/spec_helper'
+require 'rspec-system-puppet/helpers'
+require 'rspec-system-serverspec/helpers'
+include Serverspec::Helper::RSpecSystem
+include Serverspec::Helper::DetectOS
+include RSpecSystemPuppet::Helpers
+
+RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  # Enable colour
+  c.tty = true
+
+  c.include RSpecSystemPuppet::Helpers
+
+  # This is where we 'setup' the nodes before running our tests
+  c.before :suite do
+    # Install puppet
+    puppet_install
+
+    # Install modules and dependencies
+    puppet_module_install(:source => proj_root, :module_name => 'concat')
+  end
+end

spec/system/basic_spec.rb
@@ -1,0 +1,13 @@
+require 'spec_helper_system'
+
+# Here we put the more basic fundamental tests, ultra obvious stuff.
+describe "basic tests:" do
+  context 'make sure we have copied the module across' do
+    # No point diagnosing any more if the module wasn't copied properly
+    context shell 'ls /etc/puppet/modules/concat' do
+      its(:stdout) { should =~ /Modulefile/ }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+  end
+end

spec/system/concat_spec.rb
@@ -1,0 +1,55 @@
+require 'spec_helper_system'
+
+describe 'basic concat test' do
+  context 'should run successfully' do
+    pp="
+      concat { '/tmp/file':
+        owner => root,
+        group => root,
+        mode  => '0644',
+      }
+
+      concat::fragment { '1':
+        target  => '/tmp/file',
+        content => '1',
+        order   => '01',
+      }
+
+      concat::fragment { '2':
+        target  => '/tmp/file',
+        content => '2',
+        order   => '02',
+      }
+    "
+
+    context puppet_apply(pp) do
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+
+    describe file('/tmp/file') do
+      it { should be_file }
+      it { should contain '1' }
+      it { should contain '2' }
+    end
+
+    # Test that all the relevant bits exist on disk after it
+    # concats.
+    describe file('/var/lib/puppet/concat') do
+      it { should be_directory }
+    end
+    describe file('/var/lib/puppet/concat/_tmp_file') do
+      it { should be_directory }
+    end
+    describe file('/var/lib/puppet/concat/_tmp_file/fragments') do
+      it { should be_directory }
+    end
+    describe file('/var/lib/puppet/concat/_tmp_file/fragments.concat') do
+      it { should be_file }
+    end
+
+  end
+end

spec/system/empty_spec.rb
@@ -1,0 +1,27 @@
+require 'spec_helper_system'
+
+describe 'basic concat test' do
+  context 'should run successfully' do
+    pp="
+      concat { '/tmp/file':
+        owner => root,
+        group => root,
+        mode  => '0644',
+        force => true,
+      }
+    "
+
+    context puppet_apply(pp) do
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+
+    describe file('/tmp/file') do
+      it { should be_file }
+      it { should_not contain '1\n2' }
+    end
+  end
+end

spec/system/replace_spec.rb
@@ -1,0 +1,37 @@
+require 'spec_helper_system'
+
+
+describe 'file should not replace' do
+  shell('echo "file exists" >> /tmp/file')
+  context 'should fail' do
+    pp="
+      concat { '/tmp/file':
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+        replace => false,
+      }
+
+      concat::fragment { '1':
+        target  => '/tmp/file',
+        content => '1',
+        order   => '01',
+      }
+
+      concat::fragment { '2':
+        target  => '/tmp/file',
+        content => '2',
+        order   => '02',
+      }
+    "
+
+    context puppet_apply(pp) do
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+
+  end
+end

spec/system/warn_spec.rb
@@ -1,0 +1,41 @@
+require 'spec_helper_system'
+
+describe 'basic concat test' do
+  context 'should run successfully' do
+    pp="
+      concat { '/tmp/file':
+        owner => root,
+        group => root,
+        mode  => '0644',
+        warn  => true,
+      }
+
+      concat::fragment { '1':
+        target  => '/tmp/file',
+        content => '1',
+        order   => '01',
+      }
+
+      concat::fragment { '2':
+        target  => '/tmp/file',
+        content => '2',
+        order   => '02',
+      }
+    "
+
+    context puppet_apply(pp) do
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+
+    describe file('/tmp/file') do
+      it { should be_file }
+      it { should contain '# This file is managed by Puppet. DO NOT EDIT.' }
+      it { should contain '1' }
+      it { should contain '2' }
+    end
+  end
+end