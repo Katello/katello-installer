fb53739cc3e294d8fca1e4e09ed209c1460b31ae
Eric D. Helms <ericdhelms@gmail.com>
2014-01-17 12:37:23 -0500

    Squashed 'modules/kafo/' content from commit 39c90c8
    git-subtree-dir: modules/kafo
    git-subtree-split: 39c90c80ad0b903de231be1c604f78b12996aaa2

---
+++ manifests/foreman_certs.pp
@@ -0,0 +1,46 @@
+class kafo::foreman_certs (
+    $hostname    = $::certs::node_fqdn,
+    $generate    = $::certs::generate,
+    $regenerate  = $::certs::regenerate,
+    $deploy      = $::certs::deploy,
+    $ca          = $::certs::default_ca,
+    $client_cert = $::kafo::params::foreman_client_cert,
+    $client_key  = $::kafo::params::foreman_client_key,
+    $client_ca   = $::kafo::params::foreman_client_ca
+  ) {
+
+  # cert for authentication of puppetmaster against foreman
+  cert { "${::kafo::foreman_certs::hostname}-foreman-client":
+    hostname    => $::kafo::foreman_certs::hostname,
+    purpose     => client,
+    country     => $::certs::country,
+    state       => $::certs::state,
+    city        => $::certs::sity,
+    org         => 'FOREMAN',
+    org_unit    => 'PUPPET',
+    expiration  => $::certs::expiration,
+    ca          => $ca,
+    generate    => $generate,
+    regenerate    => $regenerate,
+    deploy      => $deploy,
+  }
+
+  if $deploy {
+    pubkey { $client_cert:
+      cert => Cert["${::kafo::foreman_certs::hostname}-foreman-client"],
+    }
+
+    privkey { $client_key:
+      cert => Cert["${::kafo::foreman_certs::hostname}-foreman-client"],
+    } ->
+
+    file { $client_key:
+      owner => "foreman",
+      mode => "0400"
+    }
+
+    pubkey { $client_ca:
+      cert => $ca,
+    }
+  }
+}
