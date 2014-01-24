7f9e21fe76604a57f945f2314f3b59ad5e08307d
Eric D Helms <ehelms@redhat.com>
2014-01-23 20:09:50 -0500

    Fixes issue with with Puppet 3+ calling custom functions.

--- modules/katello_installer/lib/puppet/parser/functions/find_or_create_password.rb
+++ modules/katello_installer/lib/puppet/parser/functions/find_or_create_password.rb
@@ -2,12 +2,13 @@ module Puppet::Parser::Functions
   # returns content of password file or creates random one (if not exists)
   newfunction(:find_or_create_password, :type => :rvalue) do |args|
     filename = args[0]
+
     if File.exists? filename
       puts "Loading random seed from #{filename}"
       IO.read(filename).chomp
     else
       puts "Generating new random seed in #{filename}"
-      randomhash = function_generate_password()
+      randomhash = function_generate_password([])
       File.open(filename, 'w', 0600) {|f| f.write(randomhash) }
       randomhash.chomp
     end
