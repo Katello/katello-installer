9108da73f0e516ba971108e85bdb60bd3ac51dea
Eric D. Helms <ericdhelms@gmail.com>
2014-02-10 13:56:18 -0500

    Squashed 'modules/qpid/' changes from f450601..b39fa9d
    b39fa9d Merge pull request #3 from ehelms/cleanup
    bd9c602 Clean-up and parameterizing.
    
    git-subtree-dir: modules/qpid
    git-subtree-split: b39fa9de139c4c28ce5ffda7ac8c941bc3863ace

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,41 @@
+# == Class: qpid
+#
+# Install and configure candlepin
+#
+# ===  Parameters:
+#
+# $ssl::                      Use SSL with Qpid;
+#                             default: false
+#                             type: boolean
+#
+# $ssl_port::                 SSL port to use;
+#                             default: 5671
+#                             type: string
+#
+# $ssl_cert_db::              The SSL cert database to use
+#                             type: string
+#
+# $ssl_cert_password_file::   The SSL cert password file
+#                             type: string
+#
+# $ssl_cert_name::            The SSL cert name
+#                             string: string
+#
+# $user_groups::              Additional user groups to add the qpidd user to
+#
 class qpid (
+
   $ssl                    = $qpid::params::ssl,
   $ssl_port               = $qpid::params::ssl_port,
-  # TODO: cleanup the params
-  $ssl_cert_db            = $qpid::params::ssl_cert_db,
-  $ssl_cert_password_file = $qpid::params::ssl_cert_password_file,
-  $ssl_cert_name          = $qpid::params::ssl_cert_name
+  $ssl_cert_db            = 'UNSET',
+  $ssl_cert_password_file = 'UNSET',
+  $ssl_cert_name          = 'UNSET',
+
+  $user_groups            = $qpid::params::user_groups
+
   ) inherits qpid::params {
 
   class { 'qpid::install': } ~>
-  class { 'certs::qpid': } ~>
   class { 'qpid::config': } ~>
   class { 'qpid::service': } ->
   Class['qpid']
