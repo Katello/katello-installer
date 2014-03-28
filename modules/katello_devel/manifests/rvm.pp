f00a0237d75eb8b450cf8068894b5f684e3d8428
Eric D. Helms <ericdhelms@gmail.com>
2014-03-28 12:02:54 -0400

    Update puppet modules

--- modules/katello_devel/manifests/rvm.pp
+++ modules/katello_devel/manifests/rvm.pp
@@ -5,10 +5,15 @@ class katello_devel::rvm {
 
   rvm::system_user { $katello_devel::user: ; }
 
+  $build_opts = $::operatingsystem ? {
+    'RedHat' => [],
+    default  => ['--binary']
+  }
+
   rvm_system_ruby { 'ruby-1.9.3-p448':
     ensure      => 'present',
     default_use => true,
-    build_opts  => ['--binary'],
+    build_opts  => $build_opts,
   }
 
   rvm_gem { 'bundler':
