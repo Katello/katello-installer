f322b740c58b14a9f6756c1ce3d404ada7a2ed3e
Eric D. Helms <ericdhelms@gmail.com>
2014-05-19 15:24:54 -0400

    Update puppet modules

--- modules/qpid/manifests/config.pp
+++ modules/qpid/manifests/config.pp
@@ -9,9 +9,19 @@ class qpid::config {
     groups => [$qpid::user_groups],
   }
 
+  # Qpidd 0.24+ expects the config file in this location
   file { '/etc/qpid/qpidd.conf':
     ensure  => file,
     content => template('qpid/qpidd.conf.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
+  # Qpidd 0.18 and 0.22 expects the config file in this location
+  file { '/etc/qpidd.conf':
+    ensure  => link,
+    target  => '/etc/qpid/qpidd.conf',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
