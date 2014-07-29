3c780f8f3d9673cd5e664ecf05061b81ba6e3a5c
Bryan Kearney <bkearney@redhat.com>
2014-07-29 14:51:51 -0400

    Fixes #6823 : Use kilobytes instead of human readable strings and be more permissive if mongo already exists

--- checks/disk_size.rb
+++ checks/disk_size.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-SIZE = %q(The installation requires at least 5G of storage.) 
+DISK_SIZE = %q(The installation requires at least 5G of storage.)
 
 def error_exit(message, code)
   $stderr.puts message
@@ -8,11 +8,14 @@ def error_exit(message, code)
 end
 
 begin
-    total_space = `df -H --total`.split("\n")[-1].split()[3]
-
-    # Look for a value greater than 4GB
-    error_exit(SIZE, 1) unless total_space.include?("G")
-    error_exit(SIZE, 2) if total_space.gsub("G","").to_i < 5
+    # Error out if there is not 5 gigs of free space.
+    # If mongo is installed, which is the big item, then add
+    # the current mongo space to the total
+    gb_limit = 5 * 1024 * 1024
+    mongo_dir = "/var/lib/mongodb"
+    total_space = `df -k --total --exclude-type=tmpfs`.split("\n")[-1].split()[3].to_i
+    mongo_size = File.directory?(mongo_dir) ? `du -s  /var/lib/mongodb/`.split()[0].to_i : 0
+    error_exit(DISK_SIZE, 1)  if (total_space + mongo_size) < gb_limit
 rescue
     # Eat the exception and continue
 end
