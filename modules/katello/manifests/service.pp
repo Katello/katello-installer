10bd17d3e68fbe327e36de66b761c4135214b718
Ivan Nečas <inecas@redhat.com>
2014-08-20 13:08:24 +0200

    Refs #6297 - update puppet-katello module

--- modules/katello/manifests/service.pp
+++ modules/katello/manifests/service.pp
@@ -1,13 +1,4 @@
 # Katello Services
 class katello::service {
   include pulp::service
-
-  service {'katello-jobs':
-    ensure      => running,
-    require     => Exec['foreman-rake-db:migrate'],
-    before      => Service[httpd],
-    enable      => true,
-    hasstatus   => true,
-    hasrestart  => true,
-  }
 }
