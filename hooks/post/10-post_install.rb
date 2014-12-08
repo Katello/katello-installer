024be05feabd0e3d46a04444998b28c28315f666
Stephen Benjamin <stbenjam@redhat.com>
2014-12-08 15:33:56 +0100

    fixes #8613 - support upgrades in the installer

--- hooks/post/10-post_install.rb
+++ hooks/post/10-post_install.rb
@@ -1,12 +1,5 @@
-# helpers
-def module_enabled?(name)
-  mod = @kafo.module(name)
-  return false if mod.nil?
-  mod.enabled?
-end
-
 def proxy_available?
-  module_enabled?('capsule') &&
+  Kafo::Helpers.module_enabled?(@kafo, 'capsule') &&
    (@kafo.param('capsule', 'puppet').value ||
     @kafo.param('capsule', 'puppetca').value ||
     @kafo.param('capsule', 'dhcp').value ||
@@ -14,44 +7,40 @@ def proxy_available?
     @kafo.param('capsule', 'tftp').value)
 end
 
-def read_cache_data(param)
-  YAML.load_file("/var/lib/puppet/foreman_cache_data/#{param}")
-end
+if [0,2].include?(@kafo.exit_code)
+  if !app_value(:upgrade)
+    fqdn = Facter.value(:fqdn)
 
-if [0,2].include? @kafo.exit_code
+    say "  <%= color('Success!', :good) %>"
 
-  fqdn = Facter.value(:fqdn)
+    # Fortello UI?
+    if Kafo::Helpers.module_enabled?(@kafo, 'katello')
+      say "  * <%= color('Katello', :info) %> is running at <%= color('#{@kafo.param('foreman','foreman_url').value}', :info) %>"
+      say "      Initial credentials are <%= color('#{@kafo.param('foreman', 'admin_username').value}', :info) %> / <%= color('#{@kafo.param('foreman', 'admin_password').value}', :info) %>" if @kafo.param('foreman','authentication').value == true
+    end
 
-  say "  <%= color('Success!', :good) %>"
+    # Capsule?
+    if proxy_available?
+      say "  * <%= color('Capsule', :info) %> is running at <%= color('https://#{fqdn}:#{@kafo.param('capsule', 'foreman_proxy_port').value}', :info) %>"
+    end
 
-  # Fortello UI?
-  if module_enabled?('katello')
-    say "  * <%= color('Katello', :info) %> is running at <%= color('#{@kafo.param('foreman','foreman_url').value}', :info) %>"
-    say "      Initial credentials are <%= color('#{@kafo.param('foreman', 'admin_username').value}', :info) %> / <%= color('#{@kafo.param('foreman', 'admin_password').value}', :info) %>" if @kafo.param('foreman','authentication').value == true
-  end
-
-  # Capsule?
-  if proxy_available?
-    say "  * <%= color('Capsule', :info) %> is running at <%= color('https://#{fqdn}:#{@kafo.param('capsule', 'foreman_proxy_port').value}', :info) %>"
-  end
-
-  if module_enabled?('katello')
-    say <<MSG
+    if Kafo::Helpers.module_enabled?(@kafo, 'katello')
+      say <<MSG
   * To install additional capsule on separate machine continue by running:"
 
       capsule-certs-generate --capsule-fqdn "<%= color('$CAPSULE', :info) %>" --certs-tar "<%= color('~/$CAPSULE-certs.tar', :info) %>"
 
 MSG
-  end
+    end
 
-  if module_enabled?('capsule_certs')
-    if certs_tar = @kafo.param('capsule_certs', 'certs_tar').value
-      capsule_fqdn          = @kafo.param('capsule_certs', 'capsule_fqdn').value
-      foreman_oauth_key     = read_cache_data("oauth_consumer_key")
-      foreman_oauth_secret  = read_cache_data("oauth_consumer_secret")
-      katello_oauth_secret  = read_cache_data("katello_oauth_secret")
-      org                   = @kafo.param('certs', 'org').value
-      say <<MSG
+    if Kafo::Helpers.module_enabled?(@kafo, 'capsule_certs')
+      if certs_tar = @kafo.param('capsule_certs', 'certs_tar').value
+        capsule_fqdn          = @kafo.param('capsule_certs', 'capsule_fqdn').value
+        foreman_oauth_key     = Kafo::Helpers.read_cache_data("oauth_consumer_key")
+        foreman_oauth_secret  = Kafo::Helpers.read_cache_data("oauth_consumer_secret")
+        katello_oauth_secret  = Kafo::Helpers.read_cache_data("katello_oauth_secret")
+        org                   = @kafo.param('certs', 'org').value
+        say <<MSG
 
   To finish the installation, follow these steps:
 
@@ -75,9 +64,9 @@ MSG
                     --puppetca             "<%= color('true', :info) %>"\\
                     --pulp                 "<%= color('true', :info) %>"
 MSG
+      end
     end
   end
-
   exit_code = 0
 else
   say "  <%= color('Something went wrong!', :bad) %> Check the log for ERROR-level output"
