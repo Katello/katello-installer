9150d2751f2dcfd5ba29734317bf4d943a37489f
Stephen Benjamin <stbenjam@redhat.com>
2015-04-21 13:35:50 +0200

    fixes #10221 - handle services and failures better during upgrades

--- hooks/pre/30-upgrade.rb
+++ hooks/pre/30-upgrade.rb
@@ -1,3 +1,13 @@
+def stop_services
+  Kafo::Helpers.execute('katello-service stop --exclude mongod')
+end
+
+def start_mongo
+  status = Kafo::Helpers.execute('service-wait mongod status')
+  Kafo::Helpers.execute('service-wait mongod start') unless status
+  Kafo::Helpers.execute('service-wait mongod status')
+end
+
 def migrate_candlepin
   Kafo::Helpers.execute("/usr/share/candlepin/cpdb --update --password #{Kafo::Helpers.read_cache_data('candlepin_db_password')}")
 end
@@ -27,14 +37,24 @@ end
 def upgrade_step(step)
   noop = app_value(:noop) ? ' (noop)' : ''
 
-  Kafo::Helpers.log_and_say :info, "Upgrade Step: #{step.to_s}#{noop}..."
-  send(step) unless app_value(:noop)
+  Kafo::Helpers.log_and_say :info, "Upgrade Step: #{step}#{noop}..."
+  unless app_value(:noop)
+    status = send(step)
+    fail_and_exit "Upgrade step #{step} failed. Check logs for more information." unless status
+  end
+end
+
+def fail_and_exit(message)
+  Kafo::Helpers.log_and_say :error, message
+  kafo.class.exit 1
 end
 
 if app_value(:upgrade)
   Kafo::Helpers.log_and_say :info, 'Upgrading...'
+  upgrade_step :stop_services
 
   if Kafo::Helpers.module_enabled?(@kafo, 'katello') || @kafo.param('capsule', 'pulp').value
+    upgrade_step :start_mongo
     upgrade_step :migrate_pulp
   end
 
