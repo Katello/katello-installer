63ffb72c46af790bd6d7f4908b8f8796abd1dc84
Stephen Benjamin <stbenjam@redhat.com>
2015-06-05 10:07:42 -0400

    fixes #10716 - migrate pulp in post as well

--- hooks/post/30-upgrade.rb
+++ hooks/post/30-upgrade.rb
@@ -1,9 +1,14 @@
 if app_value(:upgrade)
-  if Kafo::Helpers.module_enabled?(@kafo, 'katello')
-    noop = app_value(:noop) ? ' (noop)' : ''
+  if Kafo::Helpers.module_enabled?(@kafo, 'katello') || @kafo.param('capsule', 'pulp').value
+    Kafo::Helpers.log_and_say :info, "Upgrade Step: migrate_pulp...#{noop}"
+    Kafo::Helpers.execute('su - apache -s /bin/bash -c pulp-manage-db') unless app_value(:noop)
 
     Kafo::Helpers.log_and_say :info, "Upgrade Step: Restarting services...#{noop}"
     Kafo::Helpers.execute('katello-service restart') unless app_value(:noop)
+  end
+
+  if Kafo::Helpers.module_enabled?(@kafo, 'katello')
+    noop = app_value(:noop) ? ' (noop)' : ''
 
     Kafo::Helpers.log_and_say :info, "Upgrade Step: db:seed...#{noop}"
     Kafo::Helpers.execute('foreman-rake db:seed') unless app_value(:noop)
