fbbfc690ca9e3c068ff871adc6eb253ecae906fb
Justin Sherrill <jsherril@redhat.com>
2015-10-27 09:38:06 -0400

    pulling new pulp and capsule modules for capsules

--- modules/pulp/manifests/broker.pp
+++ modules/pulp/manifests/broker.pp
@@ -1,18 +1,17 @@
 # Set up the broker
 class pulp::broker {
+  if $pulp::messaging_transport == 'qpid' {
+    $broker_service = 'qpidd'
+  } elsif $pulp::messaging_transport == 'rabbitmq' {
+    $broker_service = 'rabbitmq-server'
+  }
+
   if $pulp::manage_broker {
     if $pulp::messaging_transport == 'qpid' {
       include ::qpid
-      $broker_service = 'qpidd'
     } elsif $pulp::messaging_transport == 'rabbitmq' {
       include ::rabbitmq
-      $broker_service = 'rabbitmq-server'
     }
-
-    Service[$broker_service] -> Service['pulp_celerybeat']
-    Service[$broker_service] -> Service['pulp_workers']
-    Service[$broker_service] -> Service['pulp_resource_manager']
-    Service[$broker_service] -> Exec['migrate_pulp_db']
   } else {
     if $pulp::messaging_transport == 'qpid' {
       include ::qpid::tools
@@ -20,4 +19,9 @@ class pulp::broker {
       Class['qpid::tools'] -> Class['pulp::service']
     }
   }
+
+  Service[$broker_service] -> Service['pulp_celerybeat']
+  Service[$broker_service] -> Service['pulp_workers']
+  Service[$broker_service] -> Service['pulp_resource_manager']
+  Service[$broker_service] -> Exec['migrate_pulp_db']
 }
