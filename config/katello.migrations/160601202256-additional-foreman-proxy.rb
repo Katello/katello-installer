55d8f6d261daac22670f78e5d03cd5a4f79e6be6
Eric D. Helms <ericdhelms@gmail.com>
2016-06-13 14:04:56 -0400

    Fixes #15390: Ignore empty answers during Capsule to Proxy migration
    Without this check, answers not found already (such as a new
    installation) are set to nil instead of the default values provided
    by the puppet module.

--- config/katello.migrations/160601202256-additional-foreman-proxy.rb
+++ config/katello.migrations/160601202256-additional-foreman-proxy.rb
@@ -1,4 +1,5 @@
 def move(name, default=nil, new_name=nil)
+  return unless answers['capsule'].key?(name)
   answers['foreman_proxy'][(new_name || name)] = answers['capsule'].delete(name) { |k| default }
 end
 
