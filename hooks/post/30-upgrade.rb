a3d0e517535b5263d867fadf7a8bc105e0f4dbc7
Justin Sherrill <jsherril@redhat.com>
2016-10-11 14:02:55 -0400

    Fixes #16869 - only run post upgrade steps once

--- hooks/post/30-upgrade.rb
+++ hooks/post/30-upgrade.rb
@@ -1,3 +1,7 @@
+require 'fileutils'
+
+STEP_DIRECTORY = '/etc/foreman-installer/applied_hooks/post/'
+
 def restart_services
   Kafo::Helpers.execute('katello-service restart')
 end
@@ -65,12 +69,29 @@ end
 def upgrade_step(step, options = {})
   noop = app_value(:noop) ? ' (noop)' : ''
   long_running = options[:long_running] ? ' (this may take a while) ' : ''
+  run_always = options.fetch(:run_always, false)
 
-  Kafo::Helpers.log_and_say :info, "Upgrade Step: #{step}#{long_running}#{noop}..."
-  unless app_value(:noop)
-    status = send(step)
-    fail_and_exit "Upgrade step #{step} failed. Check logs for more information." unless status
+  if run_always || app_value(:force_upgrade_steps) || !step_ran?(step)
+    Kafo::Helpers.log_and_say :info, "Upgrade Step: #{step}#{long_running}#{noop}..."
+    unless app_value(:noop)
+      status = send(step)
+      fail_and_exit "Upgrade step #{step} failed. Check logs for more information." unless status
+      touch_step(step)
+    end
   end
+end
+
+def touch_step(step)
+  FileUtils.mkpath(STEP_DIRECTORY) unless Dir.exists?(STEP_DIRECTORY)
+  FileUtils.touch(step_path(step))
+end
+
+def step_ran?(step)
+  File.exists?(step_path(step))
+end
+
+def step_path(step)
+  File.join(STEP_DIRECTORY, step.to_s)
 end
 
 def fail_and_exit(message)
@@ -80,10 +101,10 @@ end
 
 if app_value(:upgrade)
   if [0, 2].include?(@kafo.exit_code)
-    upgrade_step :restart_services
+    upgrade_step :restart_services, :run_always => true
 
     if Kafo::Helpers.module_enabled?(@kafo, 'katello')
-      upgrade_step :db_seed
+      upgrade_step :db_seed, :run_always => true
       upgrade_step :import_package_groups, :long_running => true
       upgrade_step :import_rpms, :long_running => true
       upgrade_step :import_distributions, :long_running => true
