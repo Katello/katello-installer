ef64a14888273ca55e5181f2eec3197c2548bf12
Stephen Benjamin <stephen@bitbin.de>
2016-12-19 10:40:08 -0500

    fixes #17507 - remove workaround for ssl.conf (#442)

--- hooks/pre/29-remove_package_httpd_conf.rb
+++ hooks/pre/29-remove_package_httpd_conf.rb
@@ -1,13 +1,10 @@
-# Some packages like mod_ssl and pulp place configuration files in
-# /etc/httpd/conf.d that contain duplicates declarations of directives
-# that the puppetlabs-apache module configured elsewhere.
+# Some packages like pulp place configuration files in /etc/httpd/conf.d that
+# contain duplicates declarations of directives that the puppetlabs-apache
+# module configured elsewhere.
 #
 # * pulp places a pulp.conf that contains a duplicate WSGI 'pulp'
 #   daemon that 05-pulps-https.conf contains.
 #
-# * mod_ssl places a 'Listen 443' directive in ssl.conf that ports.conf already
-#   has.
-
-%w(ssl.conf pulp.conf).each do |file|
+%w(pulp.conf).each do |file|
   File.delete(File.join("/etc/httpd/conf.d/", file)) if File.file?(File.join("/etc/httpd/conf.d/", file))
 end
