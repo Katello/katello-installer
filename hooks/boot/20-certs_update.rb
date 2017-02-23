3e72490b2b9c74e5b716ab5e159b6c2afc2a56b1
Stephen Benjamin <stephen@bitbin.de>
2017-02-23 10:23:51 -0500

    fixes #18322 - provide a way to reset certs to self-signed (#476)
    This PR adds an option called "--certs-reset" that will remove
    any settings related to custom certs, and then deploy the certs.
    (cherry picked from commit 2b4081ea7730092b038bbc6ef341d63d97d96a21)

--- hooks/boot/20-certs_update.rb
+++ hooks/boot/20-certs_update.rb
@@ -18,6 +18,15 @@ app_option(
   :default => false
 )
 app_option(
+  '--certs-reset',
+  :flag,
+  "This option will reset any custom certificates and use the self-signed CA " \
+  "instead. Note that any clients will need to be updated with the latest " \
+  "katello-ca-consumer RPM, and any external proxies will need to have the " \
+  "certs updated by generating a new certs tarball.",
+  :default => false
+)
+app_option(
   '--certs-skip-check',
   :flag,
   "This option will cause skipping the certificates sanity check. Use with caution",
