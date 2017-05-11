d0c1c797739cd931d9beaec573c66022273d5ec9
Chris Roberts <chrobert@redhat.com>
2017-05-10 22:51:21 -0400

    Fixes #19517 - add reset repomd to clear function
    (cherry picked from commit 825392414d6b2cbd175f3ca53b9687d5e98c10e7)

--- hooks/pre/11-clear_pulp_data_feature.rb
+++ hooks/pre/11-clear_pulp_data_feature.rb
@@ -3,6 +3,7 @@
 def clear_pulp_content
   if File.directory?('/var/lib/pulp/content')
     `rm -rf /var/lib/pulp/content/*`
+    `mongo pulp_database --eval 'db.repo_importers.update({"scratchpad": {$ne: null}}, {$set: {"scratchpad.repomd_revision": null}}, {"multi":true})'`
     logger.info 'Pulp content successfully removed.'
   else
     logger.warn 'Pulp content directory not present at \'/var/lib/pulp/content\''
