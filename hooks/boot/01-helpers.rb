ba74c283050968d6f35651cc50105f480fcfde53
Eric D. Helms <ericdhelms@gmail.com>
2017-08-14 12:31:46 -0400

    Fixes #20593 - Prevent puppet upgrade on fresh install

--- hooks/boot/01-helpers.rb
+++ hooks/boot/01-helpers.rb
@@ -10,7 +10,7 @@ class Kafo::Helpers
       mod.enabled?
     end
 
-    def log_and_say(level, message)
+    def log_and_say(level, message, do_say = true)
       style = case level
               when :error
                 'bad'
@@ -22,7 +22,7 @@ class Kafo::Helpers
 
       # \ and ' characters could cause trouble in ERB, make sure to escape them
       escaped_message = message.gsub('\\', '\\\\\\').gsub("'", %q{\\\'})
-      say "<%= color('#{escaped_message}', :#{style}) %>"
+      say "<%= color('#{escaped_message}', :#{style}) %>" if do_say
       Kafo::KafoConfigure.logger.send(level, message)
     end
 
@@ -30,26 +30,26 @@ class Kafo::Helpers
       YAML.load_file("#{puppet_dir}/foreman_cache_data/#{param}")
     end
 
-    def execute(commands)
+    def execute(commands, do_say = true)
       commands = commands.is_a?(Array) ? commands : [commands]
       results = []
       commands.each do |command|
-        results << execute_command(command)
+        results << execute_command(command, do_say)
       end
       !results.include? false
     end
 
-    def execute_command(command)
+    def execute_command(command, do_say)
       process = IO.popen("#{command} 2>&1") do |io|
         while line = io.gets
           line.chomp!
-          log_and_say(:debug, line)
+          log_and_say(:debug, line, do_say)
         end
         io.close
         if $?.success?
-          log_and_say(:debug, "#{command} finished successfully!")
+          log_and_say(:debug, "#{command} finished successfully!", do_say)
         else
-          log_and_say(:error, "#{command} failed! Check the output for error!")
+          log_and_say(:error, "#{command} failed! Check the output for error!", do_say)
         end
         $?.success?
       end
