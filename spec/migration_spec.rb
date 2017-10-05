7433310cd1ee70e7fdabfe635a8bb8c3c09c0198
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-10-05 17:04:54 +0200

    Fixes #21233 - Fix capsule to foreman-proxy-content migration
    73140936fba3992482c72635801d9d99875cc6da missed a migration and the
    tests only performed shallow copies which meant it didn't catch the
    changes.

--- spec/migration_spec.rb
+++ spec/migration_spec.rb
@@ -1,3 +1,5 @@
+# rubocop:disable BlockLength
+
 require 'spec_helper'
 require 'yaml'
 require 'kafo'
@@ -8,6 +10,13 @@ Kafo::KafoConfigure.logger = Logger.new("test.log")
 describe 'migrations' do
   %w(katello katello-devel foreman-proxy-content).each do |scenario_name|
     context "on #{scenario_name}" do
+      let(:answers) do
+        YAML.load_file(File.expand_path(File.join(CONFIG_DIR, "#{scenario_name}-answers.yaml")))
+      end
+
+      let(:config) do
+        YAML.load_file(File.expand_path(File.join(CONFIG_DIR, "#{scenario_name}.yaml")))
+      end
 
       let(:scenario) do
         {
@@ -17,16 +26,16 @@ describe 'migrations' do
         }
       end
 
-      let(:migrator) { Kafo::Migrations.new(scenario[:migrations]).run(scenario[:config].dup, scenario[:answers].dup) }
+      let(:migrator) { Kafo::Migrations.new(scenario[:migrations]).run(scenario[:config], scenario[:answers]) }
 
       it 'does not change scenario config' do
         after, = migrator
-        expect(scenario[:config]).to eq after
+        expect(config).to eq after
       end
 
       it 'does not change scenario answers' do
         _, after = migrator
-        expect(scenario[:answers]).to eq after
+        expect(answers).to eq after
       end
     end
   end
