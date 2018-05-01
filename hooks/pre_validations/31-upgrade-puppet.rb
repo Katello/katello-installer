43dd519593fbc85680934fbbaefe1566fe8b31e2
Eric D. Helms <ericdhelms@gmail.com>
2018-05-01 09:49:27 -0400

    Fixes #23470 - Reset puppet server version to ensure smooth upgrades

--- hooks/pre_validations/31-upgrade-puppet.rb
+++ hooks/pre_validations/31-upgrade-puppet.rb
@@ -1,23 +1,11 @@
-def fail_and_exit(message)
-  Kafo::Helpers.log_and_say :error, message
-  kafo.class.exit 1
-end
+unless app_value(:noop)
+  Kafo::Helpers.log_and_say :info, 'Resetting puppet server version param...'
 
-if app_value(:upgrade_puppet)
-  PUPPET_UPGRADE_COMPLETE = '/etc/foreman-installer/.puppet_5_upgrade'.freeze
-
-  fail_and_exit 'Puppet already installed and upgraded. Skipping.' if File.exist?(PUPPET_UPGRADE_COMPLETE)
-
-  katello = Kafo::Helpers.module_enabled?(@kafo, 'katello')
-  foreman_proxy_content = Kafo::Helpers.module_enabled?(@kafo, 'foreman_proxy_content')
-
-  fail_and_exit 'Puppet 4 to 5 upgrade is not currently supported for the chosen scenario.' unless katello || foreman_proxy_content
-
-  Kafo::Helpers.log_and_say :info, 'Resetting puppet params...'
-
-  if !app_value(:noop)
-    Kafo::Helpers.reset_value(param('puppet', 'server_puppetserver_version'))
+  if File.exist?('/opt/puppetlabs/puppet/bin/ruby')
+    unless Kafo::Helpers.execute("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"", false, false)
+      Kafo::Helpers.execute("yum -y reinstall puppet-agent-oauth")
+    end
   end
 
-  Kafo::Helpers.log_and_say :info, "Puppet 4 to 5 upgrade param reset, continuing with installation"
+  Kafo::Helpers.reset_value(param('puppet', 'server_puppetserver_version'))
 end
