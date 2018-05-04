f056beddb829ecdd446436c9c2a1a87ffc217bc1
Eric D. Helms <ericdhelms@gmail.com>
2018-05-04 12:36:46 -0400

    Refs #23470 - Check if puppet-agent-oauth is installed

--- hooks/pre_validations/31-upgrade-puppet.rb
+++ hooks/pre_validations/31-upgrade-puppet.rb
@@ -1,7 +1,7 @@
 unless app_value(:noop)
   Kafo::Helpers.log_and_say :info, 'Resetting puppet server version param...'
 
-  if File.exist?('/opt/puppetlabs/puppet/bin/ruby')
+  if File.exist?('/opt/puppetlabs/puppet/bin/ruby') && Kafo::Helpers.execute("rpm -q puppet-agent-oauth", false, false)
     unless Kafo::Helpers.execute("/opt/puppetlabs/puppet/bin/ruby -e \"require 'oauth'\"", false, false)
       Kafo::Helpers.execute("yum -y reinstall puppet-agent-oauth")
     end
