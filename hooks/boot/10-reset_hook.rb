74e7929e9f412fc57f6b5858b521f038b3d5f6f7
itsbill <itsbill@redhat.com>
2018-05-23 09:53:02 +0000

    Refs #15963 - katello-installer --help typos

--- hooks/boot/10-reset_hook.rb
+++ hooks/boot/10-reset_hook.rb
@@ -2,9 +2,9 @@
 app_option(
   '--reset',
   :flag,
-  "This option will drop the Katello database and clear all subsequent backend data stores." +
-  "You will lose all data! Unfortunately we\n" +
-  "can't detect a failure at the moment so you should verify the success\n" +
-  'manually. e.g. dropping can fail when DB is currently in use.',
+  'This option will drop the Katello database and clear all subsequent backend data stores. ' +
+  "You will lose all data!\nUnfortunately, we " +
+  "can't detect a failure, so you should verify success " +
+  "manually.\nDropping can fail when the DB is in use.",
   :default => false
 )
