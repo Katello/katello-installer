2a02cf645be7676a0be43e1fe5f870aae944516d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-11-20 17:33:20 +0100

    Fixes #25516 - Check LC_ALL is not set to C
    We require a UTF-8 locale and C doesn't allow to output that.

--- checks/lang.rb
+++ checks/lang.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 INVALID_LANG = %q(The LANG environment variable should not be set to C)
+INVALID_LC_ALL = %q(The LC_ALL environment variable should not be set to C)
 
 def error_exit(message, code)
   $stderr.puts message
@@ -8,3 +9,4 @@ def error_exit(message, code)
 end
 
 error_exit(INVALID_LANG, 1) if ENV["LANG"] == "C"
+error_exit(INVALID_LC_ALL, 1) if ENV["LC_ALL"] == "C"
